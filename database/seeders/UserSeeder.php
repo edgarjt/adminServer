e055a0b1a14e6da559c0b501ba5c2d2ee8690a47
DESARROLLADOR WEB <edgarjt97@gmail.com>
2022-03-14 11:01:51 -0600

    add seeder user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::where('email', 'root@email.com')->first();
+
+        if (is_null($user)) {
+            User::create([
+                'name' => 'Root',
+                'first_surname' => 'Root',
+                'last_surname' => 'Root',
+                'email' => 'root@email.com',
+                'password' => Hash::make('root')
+            ]);
+        }
+    }
+}
